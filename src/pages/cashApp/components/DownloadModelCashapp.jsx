8ac54b90f1ca0c795659d4d5ad8800cc322ca063
Richard-tech-alt <richardhawk803@gmail.com>
2025-08-14 12:42:48 -0700

    mini changes

--- src/pages/cashApp/components/DownloadModelCashapp.jsx
+++ src/pages/cashApp/components/DownloadModelCashapp.jsx
@@ -1,8 +1,43 @@
 
 import { Download, Smartphone, QrCode, X, Apple, PlayCircle } from 'lucide-react';
+import { useEffect } from 'react';
 
 
 const DownloadModal = ({ isOpen, onClose, appName,qrCode, appStoreLink  }) => {
+    useEffect(() => {
+    let scrollY = 0;
+
+    if (isOpen) {
+      scrollY = window.scrollY;
+      document.body.style.position = 'fixed';
+      document.body.style.top = `-${scrollY}px`;
+      document.body.style.left = '0';
+      document.body.style.right = '0';
+      document.body.style.overflow = 'hidden';
+      document.body.style.width = '100%';
+    } else {
+      const y = document.body.style.top;
+      document.body.style.position = '';
+      document.body.style.top = '';
+      document.body.style.left = '';
+      document.body.style.right = '';
+      document.body.style.overflow = '';
+      document.body.style.width = '';
+      window.scrollTo(0, parseInt(y || '0') * -1);
+    }
+
+    return () => {
+      const y = document.body.style.top;
+      document.body.style.position = '';
+      document.body.style.top = '';
+      document.body.style.left = '';
+      document.body.style.right = '';
+      document.body.style.overflow = '';
+      document.body.style.width = '';
+      window.scrollTo(0, parseInt(y || '0') * -1);
+    };
+  }, [isOpen]);
+
   if (!isOpen) return null;
 
   return (
